40fabc492991ff938d862055d0722fde89d15324
higor-haider <higor.haider@sumup.com>
2022-10-23 22:18:11 -0300

    Adds loop to print map values

--- main.go
+++ main.go
@@ -5,8 +5,15 @@ import "fmt"
 func main() {
 	colors := map[string]string{
 		"red":   "#ff0000",
-		"green": "4bf745",
+		"green": "#4bf745",
+		"white": "#fffff",
 	}
 
-	fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
